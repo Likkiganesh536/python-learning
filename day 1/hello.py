62951711c073df08ea865c825a130852f619f0dc
Likkiganesh536 <likkigansh3@email.com>
2026-04-23 18:17:21 +0530

    Day 1 Python basics

---
+++ day 1/hello.py
@@ -0,0 +1,15 @@
+#print hello world
+print('hello world')
+#print hi ganesh
+print('hi '+ 'ganesh')
+#input name and print hello name
+name = input('enter your name:')
+age = int(input('enter your age:'))
+print("hello ", name, age)
+#print welcome to python programming
+a='welcome '
+b='to '
+c='python '
+d='programming'
+e=a+b+c+d
+print(e)
---
+++ day 1/sum.py
@@ -0,0 +1,18 @@
+#sum of two numbers integer
+a= int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Sum is:", a + b)
+#sum of two numbers float
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Sum is:", a + b)
+#sum of two numbers complex
+a = complex(input("Enter first number: "))
+b = complex(input("Enter second number: "))
+
+print("Sum is:", a + b)
+
+
+
